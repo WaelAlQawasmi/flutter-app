379a1678a25c106d3b72906f78f8287861e5fff9
waelalq <wael.abuomr@gmail.com>
2022-10-19 21:14:28 +0200

    bottom bar

--- lib/secreens/listViow.dart
+++ lib/secreens/listViow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class listViow extends StatelessWidget {
   const listViow({Key? key}) : super(key: key);
 
@@ -6,51 +7,71 @@ class listViow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("list view"),
-        leading: IconButton(icon:Icon( Icons.menu),onPressed: (){},),
-        actions: [ IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
-          IconButton(icon: Icon(Icons.search), onPressed: () {}),
-          IconButton(icon: Icon(Icons.book), onPressed: () {})
-        ],
-        centerTitle: true,
-        backgroundColor: Colors.cyan,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20)
-          )
+        title: Text("List viow"),
+        backgroundColor: Colors.black87,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black87,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        color: Colors.black87,
+        child: IconTheme(
+          data: IconThemeData(color:Colors.white),
+          child: Row(
+            // mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                tooltip: 'Open navigation menu',
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
+              ),
+              IconButton(
+                tooltip: 'Search',
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                tooltip: 'Favorite',
+                icon: const Icon(Icons.favorite),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
       body: Container(
         margin: EdgeInsets.only(top: 10.0),
         child: ListView(
           padding: const EdgeInsets.only(top: 15),
-
           itemExtent: 78,
-          scrollDirection:Axis.vertical,
+          scrollDirection: Axis.vertical,
           reverse: false,
           shrinkWrap: true,
           children: [
             ListTile(
-
               leading: Icon(Icons.local_taxi),
               title: Text("first trip"),
               subtitle: Text("jordan- amman"),
               trailing: Icon(Icons.info_outline),
-              onTap: (){},
+              onTap: () {},
               tileColor: Colors.black12,
               // shape: RoundedRectangleBorder(
               //   borderRadius: BorderRadius.all(
               //     Radius.circular(15)
               //   )
               // ),
-            )
-            ,
+            ),
             ListTile(
               leading: Icon(Icons.local_taxi),
               title: Text("secand trip"),
               subtitle: Text("jordan- Irbid"),
               trailing: Icon(Icons.info_outline),
-              onTap: (){},
+              onTap: () {},
               tileColor: Colors.black12,
               // shape: RoundedRectangleBorder(
               //     borderRadius: BorderRadius.all(
@@ -58,23 +79,18 @@ class listViow extends StatelessWidget {
               //     )
               // ),
             ),
-
             ListTile(
               leading: Icon(Icons.local_taxi),
               title: Text("3 rd trip"),
               subtitle: Text("jordan- ajlon"),
               trailing: Icon(Icons.info_outline),
-              onTap: (){},
+              onTap: () {},
               tileColor: Colors.black12,
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(
-                    bottom: Radius.circular(15)
-                  )
-              ),
+                  borderRadius:
+                      BorderRadius.vertical(bottom: Radius.circular(15))),
             )
-
           ],
-
         ),
       ),
     );
--- trining.md
+++ trining.md
@@ -1,3 +1,9 @@
 ![myFistApp.png](img/wt-dashboard.png)
 ![myFistApp.png](img/typeofW.png)
 [icons](https://api.flutter.dev/flutter/material/Icons-class.html)
+- use to organize the code
+> alt + ctrl + l
+- use before the widget to make it child for another widget
+> alt + enter
+- use inside the widget to show the property
+> __ctrl__ + space
