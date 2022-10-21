6c316dc0c9a6d4572fdda22fa6a6b0b72002c411
waelalq <wael.abuomr@gmail.com>
2022-10-21 12:09:55 +0200

    stateful widget

--- lib/secreens/posts.dart
+++ lib/secreens/posts.dart
@@ -10,77 +10,107 @@ appBar: AppBar(title: Text("enas tegram",style: TextStyle(fontFamily:'Freehand-R
 
 ),
 backgroundColor: Colors.black87,),
-      body:
-      
-      Center(
+      body:postsViow());
+  }
+
+
+}
+
+
+
+
+class postsViow extends StatefulWidget {
+  const postsViow({Key? key}) : super(key: key);
+
+  @override
+  State<postsViow> createState() => _postsViowState();
+}
+
+
+
+
+
+class _postsViowState extends State<postsViow> {
+  @override
+  Widget build(BuildContext context) {
+    return      Center(
         child:
 
         Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children:[ buildContainer(),buildContainer(),
-        ],),
-      ),
-    );
+          ],),
+      );
+
   }
+  bool liked= true;
 
   Container buildContainer() {
     return Container(
 
-          alignment: Alignment.center,
-          width: 320,
-          height: 290,
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            border: Border.all(width: 2),
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      alignment: Alignment.center,
+      width: 320,
+      height: 290,
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        border: Border.all(width: 2),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+              width: 300.0,
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ2WhgbiWor6wAgodn1rNlDMnw5u9LnXo3PQ&usqp=CAU"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.network(
-                  width: 300.0,
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ2WhgbiWor6wAgodn1rNlDMnw5u9LnXo3PQ&usqp=CAU"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white),
-                    ),
-                    icon: Icon(
-                      Icons.favorite_border_outlined,
-                      color: Colors.black87,
-                    ),
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white),
-                    ),
-                    icon: Icon(
-                      Icons.comment_rounded,
-                      color: Colors.black87,
-                    ),
-                    onPressed: () {},
-                  ),
-                  IconButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(Colors.white),
-                    ),
-                    icon: Icon(
-                      Icons.share,
-                      color: Colors.black87,
-                    ),
-                    onPressed: () {},
-                  )
-                ],
+              IconButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStatePropertyAll<Color>(Colors.white),
+                ),
+                icon: liked?(Icon(
+                  Icons.favorite_border_outlined,
+                  color: Colors.black87,
+                )):(Icon(
+                  Icons.favorite,
+                  color: Colors.black87,
+                )),
+                onPressed: () {
+                setState(() {
+                  liked=!liked;
+                });
+                },
+              ),
+              IconButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStatePropertyAll<Color>(Colors.white),
+                ),
+                icon: Icon(
+                  Icons.comment_rounded,
+                  color: Colors.black87,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                  MaterialStatePropertyAll<Color>(Colors.white),
+                ),
+                icon: Icon(
+                  Icons.share,
+                  color: Colors.black87,
+                ),
+                onPressed: () {},
               )
             ],
-          ),
-        );
+          )
+        ],
+      ),
+    );
   }
 }
+
